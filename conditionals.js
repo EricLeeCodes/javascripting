7f476ce13702389e7f9b656119d0adec7567b295
Eric Lee <ericleecodes@gmail.com>
2024-01-18 21:58:21 -0800

    Initial commit with all of my work so far

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+const temperature = 38;
+
+if (temperature < 0) {
+	console.log("Make sure to pick out a scarf!");
+}
+
+else if (temperature < 15) {
+	console.log("Short sleeves won't cut it!");
+
+} else {
+
+console.log("Short sleeves are fine");
+
+}
+
+console.log("Now you're ready to go outside!");
---
+++ tinkering.js
@@ -0,0 +1,15 @@
+function multiply(a,b) {
+	return a * b;
+}
+
+const result = multiply(2,4);
+
+console.log(result);
+
+2 + 10 - 19 + 4 - 90 + 1;
+
+-20 + -19 - (-10) - (-1) + 24;
+
+(10/5)*4 - 20;
+
+4096 % 12;
