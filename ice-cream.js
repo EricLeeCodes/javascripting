60a383fa620d3ddc258555ffda517acb46dd2e0a
Eric Lee <ericleecodes@gmail.com>
2024-01-20 23:10:58 -0800

    ice-cream.js file and solution uploaded here

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+//Added a flavour
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+//First flavour
+console.log(iceCreamFlavours[0]);
+//Last flavour
+console.log(iceCreamFlavours[5]);
+//Total number of flavours
+console.log(iceCreamFlavours.length);
+
